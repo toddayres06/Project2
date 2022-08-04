598006d8d8720a800906d5975dcaac67066d3d80
unknown <jadentr.44@gmail.com>
2022-08-04 13:23:56 -0500

    game post api will create a new table

--- controllers/api/game.js
+++ controllers/api/game.js
@@ -1,11 +1,18 @@
 //apis for creating and joining game
 
 const router = require('express').Router();
+const Game = require('../../models/Gamemodel');
 
 //fetch for creating a game
 router.post('/', async (req, res) => {
     //here should create a new table for the game with a unique game id
     //and should send that game id back
+    try{
+    const newGame = await Game.create(req.body);
+    res.status(200).json(newGame);
+    }catch (err) {
+      res.status(400).json(err);
+    }
 });
 
 
