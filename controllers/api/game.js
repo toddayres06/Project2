687648566d5af229d23f497c8e918d0b34979eb4
unknown <jadentr.44@gmail.com>
2022-08-04 17:35:12 -0500

    changes

--- controllers/api/game.js
+++ controllers/api/game.js
@@ -4,7 +4,7 @@ const router = require('express').Router();
 const Game = require('../../models/Gamemodel');
 
 //fetch for creating a game
-router.post('/', async (req, res) => {
+router.get('/', async (req, res) => {
     //here should create a new table for the game with a unique game id
     //and should send that game id back
     try{
@@ -18,9 +18,21 @@ router.post('/', async (req, res) => {
 
 //fetch for joining a game
 router.get('/:gameId', async (req, res) => {
-  //here should check all of the avalible tables for the game id
-  //if it finds it tell the front end it is found
-  //else tell the front end there was an error
+  /*here should check all of the avalible tables for the game id
+  if it finds it tell the front end it is found
+  else tell the front end there was an error*/
+
+
+  try{
+    const gameData = await Game.findByPk(req.params.gameId);
+    if(gameData){
+      res.status(200).json(gameData)
+      return
+    }
+    res.status(404).json("sorry no game has this id")
+  }catch{
+    res.status(500).json(err);
+  }
 
 });
 
