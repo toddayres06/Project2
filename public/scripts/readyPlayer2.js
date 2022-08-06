a51d4f7d86c56a701f9c50c122cd1c430d21d06e
Adam Wagh <adamwagh@gmail.com>
2022-08-05 21:01:04 -0500

    playerturn and readyplayer2 functions built

---
+++ public/scripts/readyPlayer2.js
@@ -0,0 +1,16 @@
+const pinger = setInterval(isPlaying, 1000);
+
+function isPlaying() {
+  const options = { method: "GET" };
+
+  fetch(`http://localhost:3001/api/game/1`, options)
+    .then((response) => response.json())
+    .then((response) => {
+      if (response.playing) {
+        console.log("I can redirect when the waiting room is built");
+        //redirect to game screen
+      }
+    })
+
+    .catch((err) => console.error(err));
+}
