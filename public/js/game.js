01a1378fe594f1ec221fa7a78fb7381cd8b0bd65
Jaden <jadentr.44@gmail.com>
2022-08-08 13:29:59 -0500

    will show proper game screen based off of db

--- public/js/game.js
+++ public/js/game.js
@@ -1,6 +1,13 @@
 const gameData = localStorage.getItem("gameData");
 const gameID = JSON.parse(gameData).game_id;
 const player = JSON.parse(gameData).player;
+
+var actionPrompt = document.querySelector('#action-prompt');
+var attackBtn = document.querySelector('#attack')
+var heavyBtn = document.querySelector('#heavy-attack')
+var healBtn = document.querySelector('#heal')
+var oppTurn = document.querySelector('#oppTurn')
+
 const attackHandler = function () {
     action("attack",player,gameID)
 };
@@ -18,7 +25,7 @@ document
 .addEventListener('click', attackHandler)
 
 document
-.querySelector('#special-attack')
+.querySelector('#heavy-attack')
 .addEventListener('click', heavyAttackHandler)
 
 document
@@ -39,6 +46,7 @@ function action(action,playerNum,id) {
       .then((json) => console.log(json));
   }
 
+gameStatus()
 const statusPing = setInterval(gameStatus,2000)
 
   function gameStatus() {
@@ -48,9 +56,19 @@ const statusPing = setInterval(gameStatus,2000)
     })
     .then(data => {
         if(player == 1 && data.player1turn || player == 2 && !data.player1turn){
-            console.log("your turn")
+            actionPrompt.setAttribute ('style', 'display:block')
+            attackBtn.setAttribute ('style', 'display:inline')
+            heavyBtn.setAttribute ('style', 'display:inline')
+            healBtn.setAttribute ('style', 'display:inline')
+            oppTurn.setAttribute ('style', 'display:none')
         }
-        else{console.log("not your turn")}
+        else{
+            actionPrompt.setAttribute ('style', 'display:none')
+            attackBtn.setAttribute ('style', 'display:none')
+            heavyBtn.setAttribute ('style', 'display:none')
+            healBtn.setAttribute ('style', 'display:none')
+            oppTurn.setAttribute ('style', 'display:block')
+        }
     })
     .catch(error => console.log(error))
     
