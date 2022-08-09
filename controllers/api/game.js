ccbd9906bc2ffcadca386cd2c6944882c5c3aafc
A <andcooke@duck.com>
2022-08-08 19:39:59 -0500

    updating game screens

--- controllers/api/game.js
+++ controllers/api/game.js
@@ -16,7 +16,7 @@ router.get('/', async (req, res) => {
 
     res.status(200).json(newGame);
     // res.render('create')
-    }catch (err) {
+    } catch (err) {
       res.status(500).json(err);
     }
 });
@@ -43,7 +43,7 @@ router.get('/:gameId', async (req, res) => {
     }
     //else send a 404
     res.status(404).json("sorry no game has this id")
-  }catch{
+  }catch (err) {
     res.status(500).json(err);
   }
 });
