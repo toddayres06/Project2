c874f4ef4f5f89c0d03722143174f9c03cafafd9
A <andcooke@duck.com>
2022-08-08 22:18:17 -0500

    commented out sequelize connection

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const sequelize = require('./config/connection');
 // handlebars
 const exphbs = require('express-handlebars');
-const sequelize = require('./config/connection');
+// const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
